9b873d5729d93c452cf816d1cf86712e0b6505ae
Adam Mikulasev <adam@dealfetch.com.au>
2014-11-20 00:18:50 +1100

    fix minor syntax errors and logical reordering of bodyParser inclusion

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var path = require('path'),
     swig = require('swig'),
     compression = require('compression');
-    methodOverride = require('method-override'),
     bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
     MongoStore = require('connect-mongo')(session),
@@ -31,8 +31,8 @@ app.use(bodyParser.urlencoded({
 
 app.use(methodOverride(function(req, res){
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method
-    delete req.body._method
+    var method = req.body._method;
+    delete req.body._method;
     return method
   }
 }));
