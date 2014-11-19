cc0549f836709bfc5dbfdc6ca0ec1682bcf887c7
Adam Mikulasev <adam@dealfetch.com.au>
2014-11-19 17:33:21 +1100

    allow registration via user / password only (temporary)

--- config/passport.js
+++ config/passport.js
@@ -11,9 +11,6 @@ module.exports = function(app, env, User) {
 
   passport.deserializeUser(function(user, done) {
     done(null, user);
-    // User.findById(user, function(err, user) {
-    //   done(err, user);
-    // });
   });
 
   app.use(passport.initialize());
@@ -26,45 +23,45 @@ module.exports = function(app, env, User) {
     User.authenticate(username, password, done);
   }));
 
-  passport.use(new FacebookStrategy({
-      clientID: env.get('FACEBOOK_CLIENT_ID'),
-      clientSecret: env.get('FACEBOOK_CLIENT_SECRET'),
-      callbackURL: env.get('FACEBOOK_CALLBACK_URL')
-    },
-    function(accessToken, refreshToken, profile, done) {
-      // User.findOrCreate(..., function(err, user) {
-      //   if (err) { return done(err); }
-      //   done(null, user);
-      // });
-      done(null, profile);
-    }
-  ));
+  // passport.use(new FacebookStrategy({
+  //     clientID: env.get('FACEBOOK_CLIENT_ID'),
+  //     clientSecret: env.get('FACEBOOK_CLIENT_SECRET'),
+  //     callbackURL: env.get('FACEBOOK_CALLBACK_URL')
+  //   },
+  //   function(accessToken, refreshToken, profile, done) {
+  //     // User.findOrCreate(..., function(err, user) {
+  //     //   if (err) { return done(err); }
+  //     //   done(null, user);
+  //     // });
+  //     done(null, profile);
+  //   }
+  // ));
 
-  passport.use(new TwitterStrategy({
-      consumerKey: env.get('TWITTER_CONSUMER_KEY'),
-      consumerSecret: env.get('TWITTER_CONSUMER_SECRET'),
-      callbackURL: env.get('TWITTER_CALLBACK_URL')
-    },
-    function(token, tokenSecret, profile, done) {
-      // User.findOrCreate(..., function(err, user) {
-      //   if (err) { return done(err); }
-      //   done(null, user);
-      // });
-      done(null, profile)
-    }
-  ));
+  // passport.use(new TwitterStrategy({
+  //     consumerKey: env.get('TWITTER_CONSUMER_KEY'),
+  //     consumerSecret: env.get('TWITTER_CONSUMER_SECRET'),
+  //     callbackURL: env.get('TWITTER_CALLBACK_URL')
+  //   },
+  //   function(token, tokenSecret, profile, done) {
+  //     // User.findOrCreate(..., function(err, user) {
+  //     //   if (err) { return done(err); }
+  //     //   done(null, user);
+  //     // });
+  //     done(null, profile)
+  //   }
+  // ));
 
-  passport.use(new GoogleStrategy({
-      returnURL: env.get('GOOGLE_RETURN_URL'),
-      realm: env.get('GOOGLE_REALM')
-    },
-    function(identifier, profile, done) {
-      // User.findOrCreate({ openId: identifier }, function(err, user) {
-      //   done(err, user);
-      // });
-      done(null, profile);
-    }
-  ));
+  // passport.use(new GoogleStrategy({
+  //     returnURL: env.get('GOOGLE_RETURN_URL'),
+  //     realm: env.get('GOOGLE_REALM')
+  //   },
+  //   function(identifier, profile, done) {
+  //     // User.findOrCreate({ openId: identifier }, function(err, user) {
+  //     //   done(err, user);
+  //     // });
+  //     done(null, profile);
+  //   }
+  // ));
 
   app.post('/sessions', passport.authenticate('local', {
     successRedirect: '/',
@@ -73,33 +70,33 @@ module.exports = function(app, env, User) {
     failureFlash: 'Invalid email or password'
   }));
 
-  app.get('/auth/facebook', passport.authenticate('facebook'));
-  app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {
-      successRedirect: '/',
-      failureRedirect: '/sessions/new',
-      successFlash: 'Successfully logged in',
-      failureFlash: 'Failed to authenticate with provider'
-    })
-  );
+  // app.get('/auth/facebook', passport.authenticate('facebook'));
+  // app.get('/auth/facebook/callback',
+  //   passport.authenticate('facebook', {
+  //     successRedirect: '/',
+  //     failureRedirect: '/sessions/new',
+  //     successFlash: 'Successfully logged in',
+  //     failureFlash: 'Failed to authenticate with provider'
+  //   })
+  // );
 
-  app.get('/auth/twitter', passport.authenticate('twitter'));
-  app.get('/auth/twitter/callback',
-    passport.authenticate('twitter', {
-      successRedirect: '/',
-      failureRedirect: '/sessions/new',
-      successFlash: 'Successfully logged in',
-      failureFlash: 'Failed to authenticate with provider'
-    })
-  );
+  // app.get('/auth/twitter', passport.authenticate('twitter'));
+  // app.get('/auth/twitter/callback',
+  //   passport.authenticate('twitter', {
+  //     successRedirect: '/',
+  //     failureRedirect: '/sessions/new',
+  //     successFlash: 'Successfully logged in',
+  //     failureFlash: 'Failed to authenticate with provider'
+  //   })
+  // );
 
-  app.get('/auth/google', passport.authenticate('google'));
-  app.get('/auth/google/return',
-    passport.authenticate('google', {
-      successRedirect: '/',
-      failureRedirect: '/sessions/new',
-      successFlash: 'Successfully logged in',
-      failureFlash: 'Failed to authenticate with provider'
-    })
-  );
+  // app.get('/auth/google', passport.authenticate('google'));
+  // app.get('/auth/google/return',
+  //   passport.authenticate('google', {
+  //     successRedirect: '/',
+  //     failureRedirect: '/sessions/new',
+  //     successFlash: 'Successfully logged in',
+  //     failureFlash: 'Failed to authenticate with provider'
+  //   })
+  // );
 };
