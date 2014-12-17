93e0cf2ae125ea793fde21e2fa3027846434e46b
Adam Mikulasev <adam@dealfetch.com.au>
2014-12-17 19:36:44 +1100

    remove last of angular references (hopefully)

--- public/application.js
+++
@@ -1,7 +0,0 @@
-var mainApplicationModuleName = 'mean';
-
-var mainApplicationModule = angular.module(mainApplicationModuleName, []);
-
-angular.element(document).ready(function() {
-  angular.bootstrap(document, [mainApplicationModuleName]);
-});
